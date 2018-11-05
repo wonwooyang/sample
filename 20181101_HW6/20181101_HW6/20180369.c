23c82713bc0d9d1161aae9fa3b27de97c8cb0140
wonwooyang <wonwooyang@naver.com>
2018-11-06 02:33:27 +0900

    new one(will overwrite all)

--- 20181101_HW6/20181101_HW6/20180369.c
+++ 20181101_HW6/20181101_HW6/20180369.c
@@ -26,44 +26,93 @@ struct Human {
 };
 
 main() {
+	char *gArr[2] = { "male", "female" };
+	int chk;
+
+	// 기본값 5개 저장
 	struct Human Harr[10];
 	struct Human A = { "AAA", {2000, 1, 1}, 0, "AAAA", "00000000000" };
 	struct Human B = { "BBB", {2000, 12, 1}, 0, "BBBB", "00000000000" };
 	struct Human C = { "CCC", {2001, 1, 1}, 0, "CCCC", "00000000000" };
 	struct Human D = { "DDD", {2001, 1, 30}, 0, "DDDD", "00000000000" };
 	struct Human E = { "EEE", {2002, 1, 1}, 0, "EEEE", "00000000000" };
-	struct Human F = { "FFF", {2003, 1, 1}, 0, "FFFF", "00000000000" };
 	Harr[0] = A;
 	Harr[1] = B;
 	Harr[2] = C;
 	Harr[3] = D;
-	Harr[4] = E;
-	Harr[5] = F;
+	Harr[4] = E;	
+
+	// 값을 입력받아 Harr 에 넣음
+	int exit = 1, count = 5;
+	do
+	{
+		printf_s("\n=== 신상명세서 입력 ===\n");
+
+		printf_s("\n이름(20글자)[Default] : ");
+		scanf_s("%s", Harr[count].name, 20);
+		printf_s("\n성별(0 = male, 1 = female)[male] : ");
+		scanf_s("%d", &Harr[count].gender);
+		printf_s("\n생년월일(YYYY MM DD)[2000.1.1] : ");
+		scanf_s("%d %d %d", &Harr[count].birth.year, &Harr[count].birth.month, &Harr[count].birth.date);
+		printf_s("\n주소(30글자)[Default] : ");
+		scanf_s("%s", Harr[count].addr, 30);
+		printf_s("\n전화번호(숫자만)[000-0000-0000] : ");
+		scanf_s("%s", Harr[count].num, 20);
+
+		printf_s("\n입력 종료(0) 다음 입력(아무 숫자) : ");
+		scanf_s("%d", &exit);
+
+		count++;
+	} while (exit != 0|| count==10); 
+	// False = 0, True = 1
+	
+
+	//신상명세서 출력
+	printf_s("\n이름\t\t생년월일\t성별\t주소\t전화번호\n");
+	for (int i = 0; i < count; i++) {
+		printf_s("\n%s\t\t%d.%d.%d\t%s\t%s\t", Harr[i].name, Harr[i].birth.year, Harr[i].birth.month, Harr[i].birth.date, gArr[Harr[i].gender], Harr[i].addr);
+		for (int j = 0; j < strlen(Harr[i].num); j++) {
+			if (j == 3 || j == 7) {
+				printf_s("-%c", Harr[i].num[j]);
+			}else
+				printf_s("%c", Harr[i].num[j]);
+		}
+	}
 
 	sort(Harr, 0);
 
-	struct Human yang = { "Wonwoo yang", {1998, 5, 23}, 0, "Seoul", "01012345678" };
-	char *gArr[2] = { "male", "female" };
+	/*
+	printf_s("\n이름순 정렬(0) 생년월일순 정렬(1) : \n");
+	scanf_s("%d", &chk);
 	
-
-	printf_s("\n이름\t\t생년월일\t성별\t주소\t전화번호\n");
-	for (int i = 0; i < 10; i++) {
-		printf_s("%d", sizeof(Harr));
-		printf_s("\n%s\t%d.%d.%d\t%d\t%s\t%s\n", Harr[i].name, Harr[i].birth.year, Harr[i].birth.month, Harr[i].birth.date, gArr[Harr[i].gender], Harr[i].addr, Harr[i].num);
-
-	}
-	
+	sort(chk);
+	*/
+	printf_s("\n\n");
 }
 
-
+//0 > 이름순, 1 > 생년월일순 정렬
+//이걸 main의 Harr 값을 바꾸고 싶은데 안되네
 int sort(struct Human Harr[], int chk) {
 	struct Human temp;
 	if (chk == 0) {//이름순	L > H
+		printf_s("\n이름순 정렬\n");
 		for (int i = 1; i < 9; i++) {
 			if (strcmp(Harr[i].name, Harr[i+1].name) < 0) {
 				temp = Harr[i];
 				Harr[i] = Harr[i + 1];
 				Harr[i + 1] = temp;
+			}
+		}
+
+		printf_s("\n이름\t\t생년월일\t성별\t주소\t전화번호\n");
+		for (int i = 0; i < 5; i++) {
+			printf_s("\n%s\t\t%d.%d.%d\t%s\t%s\t", Harr[i].name, Harr[i].birth.year, Harr[i].birth.month, Harr[i].birth.date, Harr[i].gender, Harr[i].addr);
+			for (int j = 0; j < strlen(Harr[i].num); j++) {
+				if (j == 3 || j == 7) {
+					printf_s("-%c", Harr[i].num[j]);
+				}
+				else
+					printf_s("%c", Harr[i].num[j]);
 			}
 		}
 	}
